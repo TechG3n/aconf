c6b41f11542621ec910aedc213b927d78c42b68e
dkmur <dkmur74@gmail.com>
2022-05-18 23:58:28 +0200

    lets try and install

--- atlas.sh
+++ atlas.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
-# version 0.4
+# version 0.5
 
 #Version checks
-Ver55atlas="0.2"
+Ver55atlas="0.3"
 ### add webhook sender?
 
 #Create logfile
@@ -11,10 +11,10 @@ if [ ! -e /sdcard/aconf.log ] ;then
 fi
 
 logfile="/sdcard/aconf.log"
-#[[ -d /data/data/com.mad.pogodroid ]] && puser=$(ls -la /data/data/com.mad.pogodroid/ | head -n2 | tail -n1 | awk '{print $3}')
-#pdconf="/data/data/com.mad.pogodroid/shared_prefs/com.mad.pogodroid_preferences.xml"
-#[[ -d /data/data/de.grennith.rgc.remotegpscontroller ]] && ruser=$(ls -la /data/data/de.grennith.rgc.remotegpscontroller/ |head -n2 | tail -n1 | awk '{print $3}')
-#rgcconf="/data/data/de.grennith.rgc.remotegpscontroller/shared_prefs/de.grennith.rgc.remotegpscontroller_preferences.xml"
+[[ -d /data/data/com.mad.pogodroid ]] && puser=$(ls -la /data/data/com.mad.pogodroid/ | head -n2 | tail -n1 | awk '{print $3}')
+pdconf="/data/data/com.mad.pogodroid/shared_prefs/com.mad.pogodroid_preferences.xml"
+[[ -d /data/data/de.grennith.rgc.remotegpscontroller ]] && ruser=$(ls -la /data/data/de.grennith.rgc.remotegpscontroller/ |head -n2 | tail -n1 | awk '{print $3}')
+rgcconf="/data/data/de.grennith.rgc.remotegpscontroller/shared_prefs/de.grennith.rgc.remotegpscontroller_preferences.xml"
 aconf="/data/local/tmp/atlas_config.json"
 [[ -f /data/local/tmp/aconf_download ]] && aconf_download=$(cat /data/local/tmp/aconf_download | head -n1 ) && echo "`date +%Y-%m-%d_%T` download folder set to $aconf_download" >> $logfile
 
@@ -61,6 +61,8 @@ done
 
 install_atlas(){
 
+[[ -f /data/local/tmp/aconf_download ]] && aconf_download=$(cat /data/local/tmp/aconf_download | head -n1 ) && echo "`date +%Y-%m-%d_%T` download folder set to $aconf_download" >> $logfile
+
 # install 55atlas
 mount -o remount,rw /system
 until /system/bin/curl -s -k -L --fail --show-error -o /system/etc/init.d/55atlas https://raw.githubusercontent.com/dkmur/aconf/master/55atlas || { echo "`date +%Y-%m-%d_%T` Download 55atlas failed, exit script" >> $logfile ; exit 1; } ;do
@@ -70,8 +72,63 @@ chmod +x /system/etc/init.d/55atlas
 mount -o remount,ro /system
 echo "`date +%Y-%m-%d_%T` 55atlas installed" >> $logfile
 
+# download atlas
+/system/bin/rm -f /sdcard/Download/atlas.apk
+until /system/bin/curl -k -s -L --fail --show-error -o /sdcard/Download/atlas.apk $aconf_download/PokemodAtlas*.apk || { echo "`date +%Y-%m-%d_%T` Download atlas failed, exit script" >> $logfile ; exit 1; } ;do
+  sleep 2
+done
 
-echo "`date +%Y-%m-%d_%T` we should have installed atlas now :)" >> $logfile
+# pogodroid disable full daemon + stop pogodroid
+if [ -f "$pdconf" ] ;then
+  sed -i 's,\"full_daemon\" value=\"true\",\"full_daemon\" value=\"false\",g' $pdconf
+  chmod 660 $pdconf
+  chown $puser:$puser $pdconf
+  am force-stop com.mad.pogodroid
+  echo "`date +%Y-%m-%d_%T` pogodroid disabled" >> $logfile
+  # disable pd autoupdate
+  touch /sdcard/disableautopogodroidupdate
+fi
+
+# let us kill pogo as well and clear data
+pm clear com.nianticlabs.pokemongo
+am force-stop com.nianticlabs.pokemongo
+
+# Install atlas
+/system/bin/pm install -r /sdcard/Download/atlas.apk
+/system/bin/rm -f /sdcard/Download/atlas.apk
+echo "`date +%Y-%m-%d_%T` atlas installed" >> $logfile
+
+# Grant su access + settings
+auid="$(dumpsys package com.pokemod.atlas | grep userId | awk -F'=' '{print $2}')"
+magisk --sqlite "DELETE from policies WHERE package_name='com.pokemod.atlas'"
+magisk --sqlite "INSERT INTO policies (uid,package_name,policy,until,logging,notification) VALUES($auid,'com.pokemod.atlas',2,0,1,0)"
+pm grant com.pokemod.atlas android.permission.READ_EXTERNAL_STORAGE
+pm grant com.pokemod.atlas android.permission.WRITE_EXTERNAL_STORAGE
+echo "`date +%Y-%m-%d_%T` atlas granted su and settings set" >> $logfile
+
+# download atlas config file and adjust orgin to rgc setting
+until /system/bin/curl -s -k -L --fail --show-error -o /data/local/tmp/atlas_config.json $aconf_download/atlas_config.json || { echo "`date +%Y-%m-%d_%T` Download atlas config file failed, exit script" >> $logfile ; exit 1; } ;do
+  sleep 2
+done
+rgc_origin=$(grep -w 'websocket_origin' $rgcconf | sed -e 's/    <string name="websocket_origin">\(.*\)<\/string>/\1/')
+sed - i 's/dummy/$rgc_origin/g' /data/local/tmp/atlas_config.json
+
+
+# disable rgc pray everything is correct as we now loose websocket to madmin 
+#if [ -f "$rgcconf" ] ;then
+#  sed -i 's,\"autostart_services\" value=\"true\",\"autostart_services\" value=\"false\",g' $rgcconf
+#  sed -i 's,\"boot_startup\" value=\"true\",\"boot_startup\" value=\"false\",g' $rgcconf
+#  chmod 660 $rgcconf
+#  chown $ruser:$ruser $rgcconf
+#  # disable rgc autoupdate
+#  touch /sdcard/disableautorgcupdate
+#  # kill rgc
+#  am force-stop de.grennith.rgc.remotegpscontroller
+#  echo "`date +%Y-%m-%d_%T` rgc disabled" >> $logfile
+#fi
+
+### Set for reboot device
+#reboot=1
 }
 
 update_all(){
@@ -130,6 +187,11 @@ if [ $(cat /sdcard/aconf.log | grep `date +%Y-%m-%d` | grep rebooted | wc -l) -g
   echo "`date +%Y-%m-%d_%T` Device rebooted over 20 times today, atlas.sh signing out, see you tomorrow"  >> $logfile
   exit 1
 fi
+
+# download latest version file
+until /system/bin/curl -s -k -L --fail --show-error -o /data/local/tmp/aconf_versions $aconf_download/versions || { echo "`date +%Y-%m-%d_%T` Download atlas version file failed, exit script" >> $logfile ; exit 1; } ;do
+  sleep 2
+done
 
 
 for i in "$@" ;do
