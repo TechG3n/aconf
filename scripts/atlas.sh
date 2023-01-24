5f8dd37e38d996239fba4dc6ceb2a369699b3b24
dkmur <dkmur74@gmail.com>
2023-01-24 20:15:09 +0100

    correct sender version

--- scripts/atlas.sh
+++ scripts/atlas.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
-# version 2.1.14
+# version 2.1.15
 
 #Version checks
 Ver42atlas="1.5"
 Ver55atlas="1.0"
 VerMonitor="3.2.3"
-VerATVsender="1.7.6"
+VerATVsender="1.7.7"
 
 #Create logfile
 if [ ! -e /sdcard/aconf.log ] ;then
