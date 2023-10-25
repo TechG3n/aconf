6a9bc9f7233c4707d4140b9ad2c9b2913bfdba8a
Spammer <24196295+TechG3n@users.noreply.github.com>
2023-10-25 09:58:54 +0200

    As requested, set proxy only in same network as proxy. So remote ATVs aren't effected (if they don't use the same network)

--- scripts/atlas.sh
+++ scripts/atlas.sh
@@ -629,10 +629,24 @@ proxy_address=$(grep 'proxy_address' $aconf_versions | awk -F "=" '{ print $NF }
 if [[ ! -z $proxy_address ]] ;then
   proxy_get=$(settings list global | grep "http_proxy=" | awk -F= '{ print $NF }')
   if [ -z "$proxy_get" ] || [ "$proxy_get" = ":0" ]; then
-    settings put global http_proxy $proxy_address
-    sleep 2
-    su -c am broadcast -a android.intent.action.PROXY_CHANGE
-    logger "Set Proxy to $proxy_address"
+    set_proxy_only_in_same_network=$(grep 'set_proxy_only_in_same_network' $aconf_versions | awk -F "=" '{ print $NF }')
+    if [[ $set_proxy_only_in_same_network != "false" ]] ; then
+      proxy_net=$(echo $proxy_address | awk -F'.' '{print $1"."$2"."$3}')
+      local_net=$(ifconfig eth0 |grep 'inet addr' |cut -d ':' -f2 |cut -d ' ' -f1 | awk -F'.' '{print $1"."$2"."$3}')
+      if [ "$proxy_net" == "$local_net" ]; then
+        settings put global http_proxy $proxy_address
+        sleep 2
+        su -c am broadcast -a android.intent.action.PROXY_CHANGE
+        logger "Set Proxy to $proxy_address"
+      else
+        logger "Proxy not set, not in same network"
+      fi
+    else
+      settings put global http_proxy $proxy_address
+      sleep 2
+      su -c am broadcast -a android.intent.action.PROXY_CHANGE
+      logger "Set Proxy to $proxy_address"
+    fi
   fi
 fi
 
