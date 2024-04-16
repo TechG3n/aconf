8f428d8cfabfd8705750caf44eb628ad7f49d0eb
TechG3n <24196295+TechG3n@users.noreply.github.com>
2024-04-16 19:20:53 +0200

    WIP

---
+++ scripts/CosmogDetailsSender.sh
@@ -0,0 +1,182 @@
+#!/system/bin/sh
+# version 1.8.2
+
+source /data/local/aconf_versions
+logfile="/sdcard/aconf.log"
+
+#Configs
+cosmog_conf="/data/local/tmp/cosmog_config.json"
+cosmog_log="/data/local/tmp/cosmog.log"
+aconf_log="/sdcard/aconf.log"
+monitor_log="/sdcard/cosmog_monitor.log"
+android_version=`getprop ro.build.version.release | sed -e 's/\..*//'`
+
+# initial sleep for reboot
+sleep 120
+
+while true
+  do
+    if [ "$useSender" != true ] ;then
+      echo "`date +%Y-%m-%d_%T` ATVdetailsSender: sender stopped" >> $logfile && exit 1
+    fi
+
+# remove windows line ending
+    dos2unix $cosmog_conf
+
+# generic
+    MITM="cosmog"
+    RPL=$(($atvdetails_interval/60))
+    deviceName=$(cat $cosmog_conf | tr , '\n' | grep -w 'deviceName' | awk -F ":" '{ print $2 }' | tr -d \"})
+    arch=$(uname -m)
+    productmodel=$(getprop ro.product.model)
+    MITMSh=$(head -2 /system/bin/cosmog.sh | grep '# version' | awk '{ print $NF }')
+    MITM55=$([ -f /system/etc/init.d/55cosmog ] && head -2 /system/etc/init.d/55cosmog | grep '# version' | awk '{ print $NF }' || echo 'na')
+    MITM42=$([ -f /system/etc/init.d/42cosmog ] && head -2 /system/etc/init.d/42cosmog | grep '# version' | awk '{ print $NF }' || echo 'na')
+    monitor=$([ -f /system/bin/cosmog_monitor.sh ] && head -2 /system/bin/cosmog_monitor.sh | grep '# version' | awk '{ print $NF }' || echo 'na')
+    whversion=$([ -f /system/bin/ATVdetailsSender.sh ] && head -2 /system/bin/ATVdetailsSender.sh | grep '# version' | awk '{ print $NF }' || echo 'na')
+    pogo=$(dumpsys package com.nianticlabs.pokemongo | grep versionName | head -n1 | sed 's/ *versionName=//')
+    MITMv=$(dumpsys package com.sy1vi3.cosmog | grep versionName | head -n1 | sed 's/ *versionName=//')
+    temperature=$(cat /sys/class/thermal/thermal_zone0/temp | cut -c -2)
+    magisk=$(magisk -c | sed 's/:.*//')
+    macw=$([ -d /sys/class/net/wlan0 ] && ifconfig wlan0 |grep 'HWaddr' |awk '{ print ($NF) }' || echo 'na')
+    ip=$(ifconfig wlan0 |grep 'inet addr' |cut -d ':' -f2 |cut -d ' ' -f1 && ifconfig eth0 |grep 'inet addr' |cut -d ':' -f2 |cut -d ' ' -f1)
+    ext_ip=$(curl -k -s https://ifconfig.me/)
+    #mac=$(ifconfig wlan0 2>/dev/null | grep 'HWaddr' | awk '{print $5}' | cut -d ' ' -f1 && ifconfig eth0 2>/dev/null | grep 'HWaddr' | awk '{print $5}')
+    hostname=$(getprop net.hostname)
+    playstore=$(dumpsys package com.android.vending | grep versionName | head -n 1 | cut -d "=" -f 2 | cut -d " " -f 1)
+    proxyinfo=$(proxy=$(settings list global | grep "http_proxy=" | awk -F= '{ print $NF }'); [ -z "$proxy" ] || [ "$proxy" = ":0" ] && echo "none" || echo "$proxy")
+# atv performance
+    memTot=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+    memFree=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
+    memAv=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
+    memPogo=$(dumpsys meminfo 'com.nianticlabs.pokemongo' | grep -m 1 "TOTAL" | awk '{print $2}')
+    memMITM=$(dumpsys meminfo 'com.sy1vi3.cosmog:mapping' | grep -m 1 "TOTAL" | awk '{print $2}')
+    cpuL5=$(dumpsys cpuinfo | grep "Load" | awk '{ print $2 }')
+    cpuL10=$(dumpsys cpuinfo | grep "Load" | awk '{ print $4 }')
+    cpuL15=$(dumpsys cpuinfo | grep "Load" | awk '{ print $6 }')
+    numPogo=$(ls -l /sbin/.magisk/mirror/data/app/ | grep com.nianticlabs.pokemongo | wc -l)
+# aconf.log
+    reboot=$(grep 'Device rebooted' $aconf_log | wc -l)
+# cosmog config
+    authBearer=$(cat $cosmog_conf | tr , '\n' | grep -w 'authBearer' | awk -F ":" '{ print $2 }' | tr -d \"})
+    token=$(cat $cosmog_conf | tr , '\n' | grep -w 'deviceAuthToken' | awk -F ":" '{ print $2 }' | tr -d \"})
+    email=$(cat $cosmog_conf | tr , '\n' | grep -w 'email' | awk -F ":" '{ print $2 }' | tr -d \"})
+    rdmUrl=$(cat $cosmog_conf | tr , '\n' | grep -w 'rdmUrl' | awk -F "\"" '{ print $4 }')
+    onBoot=$(cat $cosmog_conf | tr , '\n' | grep -w 'runOnBoot' | awk -F ":" '{ print $2 }' | tr -d \"})
+# cosmog.log
+    a_pogoStarted=$(grep 'Launched Pokemon Go' $cosmog_log | wc -l)
+    a_injection=$(grep 'Injected successfully' $cosmog_log | wc -l)
+    a_ptcLogin=$(grep 'Logged in using ptc' $cosmog_log | wc -l)
+    a_MITMCrash=$(grep 'Agent has crashed or stopped responding' $cosmog_log | wc -l)
+    a_rdmError=$(grep 'Could not send heartbeat' $cosmog_log | wc -l)
+
+# monitor.log
+    m_noInternet=$(grep 'No internet' $monitor_log | wc -l)
+    m_noConfig=$(grep 'cosmog_config.json does not exist or is empty' $monitor_log | wc -l)
+    m_noLicense=$(grep 'Device Lost cosmog License' $monitor_log | wc -l)
+    m_MITMDied=$(grep 'cosmog must be dead, rebooting device' $monitor_log | wc -l)
+    m_pogoDied=$(grep 'Pogo must be dead, rebooting device' $monitor_log | wc -l)
+    m_deviceOffline=$(grep 'Device must be offline. Running a stop mapping service of cosmog, killing pogo and clearing junk' $monitor_log | wc -l)
+    m_noRDM=$(grep 'something wrong with RDM' $monitor_log | wc -l)
+    m_noFocus=$(grep 'Something is not right! Pogo is not in focus. Killing pogo and clearing junk' $monitor_log | wc -l)
+    m_unknown=$(grep 'Something happened! Some kind of error' $monitor_log | wc -l)
+
+# version specific metrics
+if [ $android_version -ge 9 ]; then
+    cpuSys=$(top -n 1 | grep %sys | awk 'NR == 1 {sub(/%sys/, "", $4); print $4}')
+    cpuUser=$(top -n 1 | grep %user | awk 'NR == 1 {sub(/%user/, "", $2); print $2}')
+    cpuPogoPct=$(dumpsys cpuinfo | grep 'com.nianticlabs.pokemongo' | awk '{print substr($1, 1, length($1)-1)}')
+    cpuApct=$(dumpsys cpuinfo | grep 'com.sy1vi3.cosmog' | awk 'NR == 1 {sub(/%/, "", $1); print $1}')
+    #Still not sure about these
+    diskSysPct=$(df -h | grep /dev/root | awk 'NR == 1 {sub("%", "", $5); print $5}')
+    diskDataPct=$(df -h | grep /data/media | awk 'NR == 1 {sub("%", "", $5); print $5}')
+    magisk_modules=$(ls -1 /sbin/.magisk/modules/ | xargs | sed -e 's/ /, /g')
+    mace=$(ifconfig eth0 |grep 'HWaddr' |awk '{ print ($5) }')
+else
+    cpuSys=$(top -n 1 | grep -m 1 "System" | awk '{print substr($2, 1, length($2)-2)}')
+    cpuUser=$(top -n 1 | grep -m 1 "User" | awk '{print substr($2, 1, length($2)-2)}')
+    cpuPogoPct=$(dumpsys cpuinfo | grep 'com.nianticlabs.pokemongo' | awk '{print substr($1, 1, length($1)-1)}')
+    cpuApct=$(dumpsys cpuinfo | grep 'com.sy1vi3.cosmog' | awk '{print substr($1, 1, length($1)-1)}')
+    diskSysPct=$(df -h | grep /sbin/.magisk/mirror/system | awk '{print substr($5, 1, length($5)-1)}')
+    diskDataPct=$(df -h | grep /sbin/.magisk/mirror/data | awk '{print substr($5, 1, length($5)-1)}')
+    magisk_modules=$(ls -1 /sbin/.magisk/img | xargs | sed -e 's/ /, /g' 2>/dev/null)
+    mace=$(ifconfig eth0 |grep 'HWaddr' |awk '{ print ($NF) }')
+fi
+
+# corrections
+[[ -z $temperature ]] && temperature=0
+[[ -z $cpuPogoPct ]] && cpuPogoPct=0
+[[ -z $cpuApct ]] && cpuApct=0
+
+#send data
+    curl -k -X POST ${atvdetails_receiver_user:+-u $atvdetails_receiver_user:$atvdetails_receiver_pass} ${atvdetails_receiver_host}${atvdetails_receiver_port:+:$atvdetails_receiver_port}/webhook -H "Accept: application/json" -H "Content-Type: application/json" --data-binary @- <<DATA
+{
+    "WHType": "ATVDetails",
+
+    "MITM": "${MITM}",
+    "RPL": "${RPL}",
+    "deviceName": "${deviceName}",
+    "arch": "${arch}",
+    "productmodel": "${productmodel}",
+    "MITMSh": "${MITMSh}",
+    "MITM55": "${MITM55}",
+    "MITM42": "${MITM42}",
+    "monitor": "${monitor}",
+    "whversion": "${whversion}",
+    "pogo": "${pogo}",
+    "MITMv": "${MITMv}",
+    "temperature": "${temperature}",
+    "magisk": "${magisk}",
+    "magisk_modules": "${magisk_modules}",
+    "macw": "${macw}",
+    "mace": "${mace}",
+    "ip": "${ip}",
+    "ext_ip": "${ext_ip}",
+    "hostname": "${hostname}",
+    "playstore": "${playstore}",
+    "proxyinfo": "${proxyinfo}",
+
+    "memTot": "${memTot}",
+    "memFree": "${memFree}",
+    "memAv": "${memAv}",
+    "memPogo": "${memPogo}",
+    "memMITM": "${memMITM}",
+    "cpuSys": "${cpuSys}",
+    "cpuUser": "${cpuUser}",
+    "cpuL5": "${cpuL5}",
+    "cpuL10": "${cpuL10}",
+    "cpuL15": "${cpuL15}",
+    "cpuPogoPct": "${cpuPogoPct}",
+    "cpuApct": "${cpuApct}",
+    "diskSysPct": "${diskSysPct}",
+    "diskDataPct": "${diskDataPct}",
+    "numPogo": "${numPogo}",
+
+    "reboot": "${reboot}",
+
+    "authBearer": "${authBearer}",
+    "token": "${token}",
+    "email": "${email}",
+    "rdmUrl": "${rdmUrl}",
+    "onBoot": "${onBoot}",
+
+    "a_pogoStarted": "${a_pogoStarted}",
+    "a_injection": "${a_injection}",
+    "a_ptcLogin": "${a_ptcLogin}",
+    "a_MITMCrash": "${a_MITMCrash}",
+    "a_rdmError": "${a_rdmError}",
+
+    "m_noInternet": "${m_noInternet}",
+    "m_noConfig": "${m_noConfig}",
+    "m_noLicense": "${m_noLicense}",
+    "m_MITMDied": "${m_MITMDied}",
+    "m_pogoDied": "${m_pogoDied}",
+    "m_deviceOffline": "${m_deviceOffline}",
+    "m_noRDM": "${m_noRDM}",
+    "m_noFocus": "${m_noFocus}",
+    "m_unknown": "${m_unknown}"
+}
+DATA
+
+    sleep $atvdetails_interval
+  done;
---
+++ scripts/cosmog_monitor.sh
@@ -0,0 +1,219 @@
+#!/system/bin/sh
+# version 3.3.3
+#set -x
+
+# Monitor by Oldmole && bbdoc
+
+logfile="/sdcard/cosmog_monitor.log"
+aconf="/data/local/tmp/cosmog_config.json"
+origin=$(cat $aconf | tr , '\n' | grep -w 'deviceName' | awk -F "\"" '{ print $4 }')
+android_version=`getprop ro.build.version.release | sed -e 's/\..*//'`
+cosmogdead=0
+pogodead=0
+deviceonline="0"
+emptycheck=9
+updatecheck=0
+healthchecklock=0
+stalllock=0
+
+source /data/local/aconf_versions
+export useMonitor
+export monitor_interval
+export discord_webhook
+export update_check_interval
+export debug
+update_check=$((update_check_interval/monitor_interval))
+
+#Create logfile, stolen from cosmog.sh
+if [ ! -e /sdcard/cosmog_monitor.log ] ;then
+	touch /sdcard/cosmog_monitor.log
+fi
+
+# stderr to logfile
+exec 2>> $logfile
+
+check_for_updates() {
+	[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] Checking for updates" >> $logfile
+	/system/bin/cosmog.sh -ua
+}
+
+stop_start_cosmog () {
+	am force-stop com.nianticlabs.pokemongo &  rm -rf /data/data/com.nianticlabs.pokemongo/cache/* 2>/dev/null & am force-stop com.sy1vi3.cosmog 
+	sleep 5
+	[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] Running the start mapping service of cosmog" >> $logfile
+
+	if [ $android_version -ge 9 ]; then
+		am start-foreground-service com.sy1vi3.cosmog/com.sy1vi3.cosmog.services.MappingService
+	else
+		am startservice com.sy1vi3.cosmog/com.sy1vi3.cosmog.services.MappingService
+	fi
+	
+	sleep 1
+}
+
+stop_pogo () {
+	am force-stop com.nianticlabs.pokemongo & rm -rf /data/data/com.nianticlabs.pokemongo/cache/* 2>/dev/null
+	sleep 5
+	[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] Killing pogo and clearing junk" >> $logfile
+}
+
+send_webhook () {
+	issue=$1;
+	action=$2;
+	curl -k -X POST $atvdetails_receiver_host:$atvdetails_receiver_port/webhook -H "Accept: application/json" -H "Content-Type: application/json" --data-binary @- <<DATA
+        {
+            "WHType": "ATVMonitor",
+            "deviceName": "${origin}",
+            "issue": "${issue}",
+            "action": "${action}",
+            "script": "cosmog_monitor.sh"
+        }
+DATA
+}
+
+
+echo "`date +%Y-%m-%d_%T` [MONITORBOT] Starting cosmog data monitor in 5 mins, loop is $monitor_interval seconds" >> $logfile
+sleep 300
+while :
+do
+	[[ $useMonitor == "false" ]] && echo "`date +%Y-%m-%d_%T` cosmog_monitor stopped" >> $logfile && exit 1
+
+	until ping -c1 8.8.8.8 >/dev/null 2>/dev/null
+	do
+		[[ $( awk '/./{line=$0} END{print line}' $logfile | grep 'No internet' | wc -l) != 1 ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] No internet, pay the bill?" >> $logfile
+		sleep 60
+	done
+
+	[[ -z $origin ]] && origin=$(cat $aconf | tr , '\n' | grep -w 'deviceName' | awk -F "\"" '{ print $4 }')
+
+        updatecheck=$(($updatecheck+1))
+        if [[ $updatecheck -gt $update_check ]] ;then
+		echo  "`date +%Y-%m-%d_%T` [MONITORBOT] Checking cosmog and Pogo for update" >> $logfile
+		updatecheck=0
+		check_for_updates
+	fi
+
+	if [ -d /data/data/com.sy1vi3.cosmog ] && [ -s /data/local/tmp/cosmog_config.json ]
+	then
+		[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] cosmog_config.json looks good" >> $logfile
+	else
+		echo "`date +%Y-%m-%d_%T` [MONITORBOT] cosmog_config.json does not exist or is empty! Let's fix that" >> $logfile
+		[[ ! -z $discord_webhook ]] && [[ $recreate_cosmog_config != "false" ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: re-creating cosmog config\"}" $discord_webhook &>/dev/null
+		/system/bin/cosmog.sh -ic
+		[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] Fixed config" >> $logfile
+		stop_start_cosmog
+		sleep $monitor_interval
+		continue
+
+	fi
+
+
+	dumpsys activity services | grep -e "MappingService" > /dev/null 2>&1
+	devicestatus=$(echo $?)
+	emptycheck="9$devicestatus"
+
+	not_licensed=$(tail -n 100 /data/local/tmp/cosmog.log | grep -c -E "Not licensed|doesn't have a valid license")
+
+	if [ -f /sdcard/not_licensed ] && [ $not_licensed -gt 0 ]
+	then
+		echo "`date +%Y-%m-%d_%T` [MONITORBOT] Still unlicensed, exiting" >> $logfile
+	elif [ $not_licensed -gt 0 ]
+	then
+		echo "`date +%Y-%m-%d_%T` [MONITORBOT] Device Lost cosmog License" >> $logfile
+		[[ ! -z $discord_webhook ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: UNLICENSED !!! Check cosmog Dashboard\"}" $discord_webhook &>/dev/null
+		[[ $useSender == "true" ]] && send_webhook "Lost Licence" "No action"
+		touch /sdcard/not_licensed
+
+	elif [ -f /sdcard/not_licensed ] && [ $not_licensed -eq 0 ]
+	then
+	    echo "`date +%Y-%m-%d_%T` [MONITORBOT] Device got License again. Recovering" >> $logfile
+        [[ ! -z $discord_webhook ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: Device got cosmog license again\"}" $discord_webhook &>/dev/null
+		rm /sdcard/not_licensed
+
+        elif [ $emptycheck != 9 ] && [ $devicestatus != $deviceonline ] && [ $cosmogdead == 2 ]
+        then
+			echo "`date +%Y-%m-%d_%T` [MONITORBOT] cosmog must be dead, rebooting device" >> $logfile
+			[[ $useSender == "true" ]] && send_webhook "cosmog Dead" "Reboot"
+			[[ ! -z $discord_webhook ]] && [[ $cosmog_died != "false" ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: cosmog died, reboot\"}" $discord_webhook &>/dev/null
+			reboot
+        elif [ $emptycheck != 9 ] && [ $pogodead == 2 ]
+        then
+            echo "`date +%Y-%m-%d_%T` [MONITORBOT] Pogo must be dead, rebooting device" >> $logfile
+			[[ $useSender == "true" ]] && send_webhook "Pogo Dead" "Reboot"
+   	        [[ ! -z $discord_webhook ]] && [[ $pogo_died != "false" ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: pogo died, reboot\"}" $discord_webhook &>/dev/null
+            reboot
+
+	elif [ $emptycheck != 9 ] && [ $devicestatus != $deviceonline ] && [ $cosmogdead != 2 ]
+	then
+		echo "`date +%Y-%m-%d_%T` [MONITORBOT] Device must be offline. Running a stop mapping service of cosmog, killing pogo and clearing junk" >> $logfile
+		[[ $useSender == "true" ]] && send_webhook "Device Offline" "Kill Pogo and Clear Junk"
+		[[ ! -z $discord_webhook ]] && [[ $device_offline != "false" ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: device offline, restarting cosmog and pogo\"}" $discord_webhook &>/dev/null
+		stop_start_cosmog
+		cosmogdead=$((cosmogdead+1))
+		[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] Done" >> $logfile
+
+	elif [ $emptycheck == 9 ]
+	then
+		echo "`date +%Y-%m-%d_%T` [MONITORBOT] Couldn't check status, something wrong with RDM?" >> $logfile
+		[[ ! -z $discord_webhook ]] && [[ $unable_check_status != "false" ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: unable to check status\"}" $discord_webhook &>/dev/null
+
+	elif [ $deviceonline == $devicestatus ]
+	then
+		[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] cosmog mapping service is running" >> $logfile
+		cosmogdead=0
+		focusedapp=$(dumpsys window windows | grep -E 'mFocusedApp'| cut -d / -f 1 | cut -d " " -f 7)
+		if [ "$focusedapp" != "com.nianticlabs.pokemongo" ]
+		then
+			echo "`date +%Y-%m-%d_%T` [MONITORBOT] Something is not right! Pogo is not in focus. Killing pogo and clearing junk" >> $logfile
+		    [[ $useSender == "true" ]] && send_webhook "Pogo not in Focus" "Kill Pogo and Clear Junk"
+			[[ ! -z $discord_webhook ]] && [[ $pogo_not_focused != "false" ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: pogo not in focus, Killing and clearing junk\"}" $discord_webhook &>/dev/null
+			stop_pogo
+			pogodead=$((pogodead+1))
+			[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] Done" >> $logfile
+		else
+			[[ $debug == "true" ]] && echo "`date +%Y-%m-%d_%T` [MONITORBOT] Pogo in focus, all good" >> $logfile
+			pogodead=0
+		fi
+	else
+		echo "`date +%Y-%m-%d_%T` [MONITORBOT] Something happened! Some kind of error" >> $logfile
+		[[ $useSender == "true" ]] && send_webhook "Unknown Error" "No action"
+		[[ ! -z $discord_webhook ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: no clue what happend, but its not good\"}" $discord_webhook &>/dev/null
+	fi
+
+	#get count of "[HEALTH CHECK] xx seconds since last ping." errors
+	lastlog=$(tail -n 200 /data/local/tmp/cosmog.log)
+	healthcheckcount=$(echo "$lastlog" | grep -E '\[HEALTH CHECK\] ([0-9]+) seconds since last ping\.' | wc -l)
+	crithealthcount=$(echo "$lastlog" | grep -E '\[HEALTH CHECK\] ([0-9]+) seconds since last ping\.' | awk '{if ($3 >= 30) count++} END {print count+0}')
+	successcount=$(echo "$lastlog" | grep -E 'I \| Worker' | wc -l)
+	if [ $healthcheckcount -ge 10 ] && [ $healthcheckcount -ge $successcount ] || [ $crithealthcount -ge 4 ] && [ $successcount -le 40 ] ; then
+		if [[ $healthchecklock == 1 ]]; then
+			#skip restart
+			healthchecklock=0
+		else
+			stop_pogo
+			echo "`date +%Y-%m-%d_%T` [MONITORBOT] Found $healthcheckcount Health Errors, $successcount successfull jobs and $crithealthcount crits - restarting Pogo" >> $logfile
+			[[ ! -z $discord_webhook ]] && [[ $healthcheck_errors != "false" ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: Found $healthcheckcount Health Errors, $successcount successfull jobs and $crithealthcount crits - restarting Pogo\"}" $discord_webhook &>/dev/null
+			healthchecklock=1
+		fi
+	else
+		healthchecklock=0
+	fi
+
+	# Instance 2's loop has been stalled for over a minute.. Restarting instance...
+	stalled=$(echo "$lastlog" | grep -E 'loop has been stalled for over a minute' | wc -l )
+	if [ $stalled -ge 1 ]; then
+		if [[ $stalllock -ge 1 ]]; then
+			#skip restart
+			stalllock=$((stalllock - 1))
+		else
+			echo "`date +%Y-%m-%d_%T` [MONITORBOT] One instance stalled - restarting cosmog" >> $logfile
+			stop_start_cosmog
+			[[ ! -z $discord_webhook ]] && curl -S -k -L --fail --show-error -F "payload_json={\"content\": \"__**$origin**__: One instance stalled - restarting cosmog\"}" $discord_webhook &>/dev/null
+			stalllock=2
+		fi
+	else
+		stalllock=$((stalllock - 1))
+	fi
+		
+	sleep $monitor_interval
+done
