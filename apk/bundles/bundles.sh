16e8a763d709ee4ee2351276cb1568f689b36646
TechG3n <24196295+TechG3n@users.noreply.github.com>
2024-10-16 16:03:09 +0200

    Get cosmog lib while downloading new version

--- apk/bundles/bundles.sh
+++ apk/bundles/bundles.sh
@@ -4,6 +4,7 @@
 download_url="https://mirror.unownhash.com/apks"
 
 output_dir="$(dirname $(pwd))"
+module_dir="${output_dir%/*}/modules"
 
 if [[ -n "$download_url" ]]; then
     read -p "Do you want to download the APK files from download_url? (y/n): " download_choice
@@ -11,6 +12,7 @@ if [[ -n "$download_url" ]]; then
         rm *.apkm 2>/dev/null
         rm -r META-INF
         read -p "Which version do you want to download? (e.g., 0.329.1): " version
+        read -p "Do you also want the cosmog lib? (y/n): " coslib
 
         url_v8a="${download_url}/com.nianticlabs.pokemongo_arm64-v8a_${version}.apkm"
         url_v7a="${download_url}/com.nianticlabs.pokemongo_armeabi-v7a_${version}.apkm"
@@ -38,13 +40,18 @@ version=$(echo $file_v8a | grep -oP '(?<=_)[0-9]+\.[0-9]+\.[0-9]+')
 # Check if the v8a file exists
 if [[ -f "$file_v8a" ]]; then
     echo "Unzipping and renaming $file_v8a"
-    unzip $file_v8a 1>/dev/null
+    unzip -o $file_v8a 1>/dev/null
     mv base.apk pokemongo_arm64-v8a_${version}_base.apk
     mv split_config.arm64_v8a.apk pokemongo_arm64-v8a_${version}_split.apk
+    #extract cosmog lib
+    if [[ $coslib == "y" ]]; then
+        unzip -o pokemongo_arm64-v8a_${version}_split.apk 1>/dev/null
+        mv lib/arm64-v8a/libNianticLabsPlugin.so "$module_dir/libNianticLabsPlugin.so_${version}"
+    fi
     # Move the final files to the target directory
     mv pokemongo_arm64-v8a_${version}_*.apk "$output_dir/"
     # Delete all files that are not the script itself
-    find . -type f ! -name "*.sh" -exec rm -rf {} +
+    #find . -type f ! -name "*.sh" -exec rm -rf {} +
 else
     echo "File $file_v8a not found. Skipping."
 fi
@@ -52,9 +59,14 @@ fi
 # Check if the v7a file exists
 if [[ -f "$file_v7a" ]]; then
     echo "Unzipping and renaming $file_v7a"
-    unzip $file_v7a 1>/dev/null
+    unzip -o $file_v7a 1>/dev/null
     mv base.apk pokemongo_armeabi-v7a_${version}_base.apk
-    mv split_config.armeabi-v7a.apk pokemongo_armeabi-v7a_${version}_split.apk
+    mv split_config.armeabi_v7a.apk pokemongo_armeabi-v7a_${version}_split.apk
+    #extract cosmog lib
+    #if [[ $coslib == "y" ]]; then
+    #    unzip -o pokemongo_armeabi-v7a_${version}_split.apk 1>/dev/null
+    #    mv lib/armeabi-v7a/libNianticLabsPlugin.so "$module_dir/libNianticLabsPlugin.so_${version}"
+    #fi
     # Move the final files to the target directory
     mv pokemongo_armeabi-v7a_${version}_*.apk "$output_dir/"
     # Delete all files that are not the script itself
