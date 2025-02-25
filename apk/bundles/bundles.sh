504bcb98da0a74871685f461e462b602b62531bc
TechG3n <24196295+TechG3n@users.noreply.github.com>
2025-02-25 14:27:21 +0100

    download latest version and change version file

--- apk/bundles/bundles.sh
+++ apk/bundles/bundles.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
+# version 0.2
 
 # Base URL for the download
 download_url="https://mirror.unownhash.com/apks"
 
-output_dir="$(dirname $(pwd))"
+# Determine the absolute path of this script and change directory to its location.
+script_path="$(readlink -f "$0")"                 # Get the full path of the script.
+script_dir="$(dirname "$script_path")"             # Extract the directory from the script path.
+cd "$script_dir" || { echo "Failed to change directory to the script's location."; exit 1; }
+
+output_dir="$(dirname "$script_dir")"
 module_dir="${output_dir%/*}/modules"
+version_file="$(dirname "$output_dir")/versions"
+
+# Initialize flags to track if v7 and v8 files have been processed.
+v7=false
+v8=false
 
 if [[ -n "$download_url" ]]; then
-    read -p "Do you want to download the APK files from download_url? (y/n): " download_choice
-    if [[ "$download_choice" =~ ^(y|Y|Yes|yes)$ ]]; then
         rm *.apkm 2>/dev/null
-        rm -r META-INF
-        read -p "Which version do you want to download? (e.g., 0.329.1): " version
-        read -p "Do you also want the cosmog lib? (y/n): " coslib
+        rm -r META-INF 2>/dev/null
 
+        # Retrieve the newest version
+        NewestVersion=$(curl -s "https://mirror.unownhash.com/index.html" | grep -oP 'com\.nianticlabs\.pokemongo_arm64-v8a_\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n 1)
+
+        if [[ -n "$NewestVersion" ]]; then
+            read -p "Do you want to download the newest version ($NewestVersion)? [Y/n]: " confirmNewest
+            if [[ "$confirmNewest" =~ ^(Y|y|Yes|yes)?$ ]]; then
+                version="$NewestVersion"
+            else
+                read -p "Which version do you want to download? (e.g., 0.329.1): " version
+            fi
+        else
+            read -p "Which version do you want to download? (e.g., 0.329.1): " version
+        fi
+
+        read -p "Do you also want the cosmog lib? (y/n): " coslib
+        coslib=${coslib:-n}
+        read -p "Do you want to update your versions file to the new version? (y/n): " version_choice
+        version_choice=${version_choice:-n}
+
+        # Construct URLs
         url_v8a="${download_url}/com.nianticlabs.pokemongo_arm64-v8a_${version}.apkm"
         url_v7a="${download_url}/com.nianticlabs.pokemongo_armeabi-v7a_${version}.apkm"
 
@@ -26,9 +53,9 @@ if [[ -n "$download_url" ]]; then
         until wget -q -O "com.nianticlabs.pokemongo_armeabi-v7a_${version}.apkm" "$url_v7a" || echo "Couldn't finde that version" ;do
             sleep 2
         done
-    fi
 else
-    echo "No download URL provided. Skipping download."
+    echo "No download URL provided. Exit."
+    exit 1
 fi
 
 file_v8a=$(ls *v8a*.apkm 2>/dev/null | grep -v "base\|split")
@@ -44,7 +71,7 @@ if [[ -f "$file_v8a" ]]; then
     mv base.apk pokemongo_arm64-v8a_${version}_base.apk
     mv split_config.arm64_v8a.apk pokemongo_arm64-v8a_${version}_split.apk
     #extract cosmog lib
-    if [[ $coslib == "y" ]]; then
+    if [[ "$coslib" =~ ^(y|Y|Yes|yes)$ ]]; then
         unzip -o pokemongo_arm64-v8a_${version}_split.apk 1>/dev/null
         mv lib/arm64-v8a/libNianticLabsPlugin.so "$module_dir/libNianticLabsPlugin.so_${version}"
     fi
@@ -52,6 +79,7 @@ if [[ -f "$file_v8a" ]]; then
     mv pokemongo_arm64-v8a_${version}_*.apk "$output_dir/"
     # Delete all files that are not the script itself
     #find . -type f ! -name "*.sh" -exec rm -rf {} +
+    v8=true
 else
     echo "File $file_v8a not found. Skipping."
 fi
@@ -71,8 +99,19 @@ if [[ -f "$file_v7a" ]]; then
     mv pokemongo_armeabi-v7a_${version}_*.apk "$output_dir/"
     # Delete all files that are not the script itself
     find . -type f ! -name "*.sh" -exec rm -rf {} +
+    v7=true
 else
     echo "File $file_v7a not found. Skipping."
+fi
+
+if $v7 && $v8; then
+    if [[ "$version_choice" =~ ^(y|Y|Yes|yes)$ ]]; then
+        sed -i "s/^pogo=.*/pogo=$version/" "$version_file"
+        if [[ "$coslib" =~ ^(y|Y|Yes|yes)$ ]]; then
+            sed -i "s/^cosmog_libVerion=.*/cosmog_libVerion=\"$version\"/" "$version_file"
+        fi
+        echo "Version file updated."
+    fi
 fi
 
 echo "Done! Files have been moved to $output_dir."
