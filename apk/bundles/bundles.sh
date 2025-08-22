d8db1e6ce9a6cd89949df9de338fa1cec254411f
TechG3n <24196295+TechG3n@users.noreply.github.com>
2025-08-22 13:11:56 +0200

    Drop 32bit / v7a
    Quick fix from mobile

--- apk/bundles/bundles.sh
+++ apk/bundles/bundles.sh
@@ -14,7 +14,7 @@ module_dir="${output_dir%/*}/modules"
 version_file="$(dirname "$output_dir")/versions"
 
 # Initialize flags to track if v7 and v8 files have been processed.
-v7=false
+#v7=false
 v8=false
 
 if [[ -n "$download_url" ]]; then
@@ -42,24 +42,24 @@ if [[ -n "$download_url" ]]; then
 
         # Construct URLs
         url_v8a="${download_url}/com.nianticlabs.pokemongo_arm64-v8a_${version}.apkm"
-        url_v7a="${download_url}/com.nianticlabs.pokemongo_armeabi-v7a_${version}.apkm"
+        #url_v7a="${download_url}/com.nianticlabs.pokemongo_armeabi-v7a_${version}.apkm"
 
         echo "Downloading ${url_v8a}..."
         until wget -q -O "com.nianticlabs.pokemongo_arm64-v8a_${version}.apkm" "$url_v8a" || echo "Couldn't finde that version" ;do
             sleep 2
         done
 
-        echo "Downloading ${url_v7a}..."
-        until wget -q -O "com.nianticlabs.pokemongo_armeabi-v7a_${version}.apkm" "$url_v7a" || echo "Couldn't finde that version" ;do
-            sleep 2
-        done
+        #echo "Downloading ${url_v7a}..."
+        #until wget -q -O "com.nianticlabs.pokemongo_armeabi-v7a_${version}.apkm" "$url_v7a" || echo "Couldn't finde that version" ;do
+        #    sleep 2
+        #done
 else
     echo "No download URL provided. Exit."
     exit 1
 fi
 
 file_v8a=$(ls *v8a*.apkm 2>/dev/null | grep -v "base\|split")
-file_v7a=$(ls *v7a*.apkm 2>/dev/null | grep -v "base\|split")
+#file_v7a=$(ls *v7a*.apkm 2>/dev/null | grep -v "base\|split")
 
 # Extract the version number (from file_v8a)
 version=$(echo $file_v8a | grep -oP '(?<=_)[0-9]+\.[0-9]+\.[0-9]+')
@@ -85,26 +85,26 @@ else
 fi
 
 # Check if the v7a file exists
-if [[ -f "$file_v7a" ]]; then
-    echo "Unzipping and renaming $file_v7a"
-    unzip -o $file_v7a 1>/dev/null
-    mv base.apk pokemongo_armeabi-v7a_${version}_base.apk
-    mv split_config.armeabi_v7a.apk pokemongo_armeabi-v7a_${version}_split.apk
+#if [[ -f "$file_v7a" ]]; then
+#    echo "Unzipping and renaming $file_v7a"
+#    unzip -o $file_v7a 1>/dev/null
+#    mv base.apk pokemongo_armeabi-v7a_${version}_base.apk
+#    mv split_config.armeabi_v7a.apk pokemongo_armeabi-v7a_${version}_split.apk
     #extract cosmog lib
     #if [[ $coslib == "y" ]]; then
     #    unzip -o pokemongo_armeabi-v7a_${version}_split.apk 1>/dev/null
     #    mv lib/armeabi-v7a/libNianticLabsPlugin.so "$module_dir/libNianticLabsPlugin.so_${version}"
     #fi
     # Move the final files to the target directory
-    mv pokemongo_armeabi-v7a_${version}_*.apk "$output_dir/"
+#    mv pokemongo_armeabi-v7a_${version}_*.apk "$output_dir/"
     # Delete all files that are not the script itself
-    find . -type f ! -name "*.sh" -exec rm -rf {} +
-    v7=true
-else
-    echo "File $file_v7a not found. Skipping."
-fi
+#    find . -type f ! -name "*.sh" -exec rm -rf {} +
+#    v7=true
+#else
+#    echo "File $file_v7a not found. Skipping."
+#fi
 
-if $v7 && $v8; then
+if $v8; then
     if [[ "$version_choice" =~ ^(y|Y|Yes|yes)$ ]]; then
         sed -i "s/^pogo=.*/pogo=$version/" "$version_file"
         if [[ "$coslib" =~ ^(y|Y|Yes|yes)$ ]]; then
